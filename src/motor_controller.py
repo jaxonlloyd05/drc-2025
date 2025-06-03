d371c020127ded9de2a4d7c7450c0da13dabf03d
Jaxon <jaxonlloyd05@gmail.com>
2025-06-03 14:44:37 +1000

    testing servo without motor

--- src/motor_controller.py
+++ src/motor_controller.py
@@ -5,13 +5,13 @@ class DRCMotorController:
     def __init__(self, motorPin, servoPin):
         # GPIO13 & GPIO12 preferable
         Device.pin_factory = RPiGPIOFactory()
-        self.motor = PWMOutputDevice(motorPin, frequency=50)
+        # self.motor = PWMOutputDevice(motorPin, frequency=50)
         self.servo = Servo(servoPin) 
         pass
 
     def setDrivingMotor(self, speed):
         # speed taken in between 0 and 1 - still to decide how to do this
-        self.motor.value = speed
+        # self.motor.value = speed
         pass
 
     def setServoMotor(self, angle):
